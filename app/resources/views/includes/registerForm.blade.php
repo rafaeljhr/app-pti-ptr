b6fefe24b55a292203299823f3e4d02aa0a0fde7
Francisco Pimenta <xisco.pimenta@gmail.com>
2022-03-12 22:22:58 +0000

    Adicão route register e form register
    + correcao css

---
+++ app/resources/views/includes/registerForm.blade.php
@@ -0,0 +1,23 @@
+<div class="container">
+    <form class="form-signin">
+        <img class="mb-4" src="images/logo.png" alt="EcoSmart Logo">
+
+        <h1 class="h3 mb-3 font-weight-normal">Junte-se ao grupo EcoSmart!</h1>
+        <label for="inputName" class="sr-only">Nome</label>
+
+        <input type="text" id="inputName" class="form-control" placeholder="João Carvalho" required="" autofocus="">
+        <label for="inputTel" class="sr-only">Telemóvel</label>
+
+        <input type="number" id="inputTel" class="form-control" placeholder="987654321" size="9" required="" autofocus="">
+        <label for="inputNIF" class="sr-only">NIF</label>
+
+        <input type="number" id="inputNIF" class="form-control" placeholder="123456789" size="9" required="" autofocus="">
+        <label for="inputAdress" class="sr-only">Morada</label>
+
+        <input type="text" id="inputAdress" class="form-control" placeholder="Rua Avenida Nº7 6ºE" maxlength="200" required="" autofocus="">
+        <label for="password" class="sr-only">Password</label>
+
+        <input type="password" id="password" class="form-control" placeholder="Password" required="" autofocus="">
+        <button class="btn btn-lg btn-primary btn-block" type="submit">Registar</button>
+    </form>
+</div>
---
+++ app/resources/views/register.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.page_default')
+
+@section('background')
+    
+    @parent
+    @include('includes.registerForm') 
+    
+@endsection
+
+
