ebb6d9b04b55697b1223dbd8f4320e3a3c70a6c8
Stack-overflow-abuser <mmd.miguel@gmail.com>
2022-03-13 13:22:12 +0000

    implementacao de butoes para o tipo de login e melhoramento do layout da pagina de login

--- app/resources/views/includes/loginForm.blade.php
+++ app/resources/views/includes/loginForm.blade.php
@@ -1,15 +1,30 @@
-<div class="container">
+<div class="container py-5 h-100">
     <form class="form-signin">
         <div class="limit-width mx-auto">  
             <img class="logo" src="images/logo.png" alt="EcoSmart Logo">
 
-            <h1 class="h3 mb-2 font-weight-normal">Autentique-se para usufruir  de todas as funcionalidades que temos para oferecer!</h1>
+            <h1 class="h3 mb-2 font-weight-normal">Autentique-se para usufruir  de todas as funcionalidades!</h1>
 
-            <label for="loginName" class="sr-only text-light"><h4>Nome de utilizador</h4></label>
-            <input type="text" id="loginName" class="form-control form-control-lg mb-2" placeholder="Nome de utilizador" required="" autofocus="">
+            <div class="form-check form-check-inline">
+                <input class="form-check-input" type="radio" name="inlineRadioOptions" id="consumidorSelect" value="optionConsumidor">
+                <label class="form-check-label" for="inlineRadio1">Consumidor</label>
+            </div>
 
+            <div class="form-check form-check-inline">
+                <input class="form-check-input" type="radio" name="inlineRadioOptions" id="transportadoraSelect" value="optionTransportadora">
+                <label class="form-check-label" for="inlineRadio1">Transportadora</label>
+            </div>
+
+            <div class="form-check form-check-inline">
+                <input class="form-check-input" type="radio" name="inlineRadioOptions" id="fornecedorSelect" value="optionFornecedor">
+                <label class="form-check-label" for="inlineRadio1">Fornecedora</label>
+            </div>
+            <div>
+            <label for="loginName" class="sr-only text-light"><h4>Email</h4></label>
+            <input type="text" id="loginName" class="form-control form-control-lg" placeholder="Nome de utilizador" required="" autofocus="">
+            </div>
             <label for="password" class="sr-only text-light"><h4>Password</h4></label>
-            <input type="password" id="password" class="form-control form-control-lg mb-2" placeholder="Password" required="" autofocus="">
+            <input type="password" id="password" class="form-control form-control-lg" placeholder="Password" required="" autofocus="">
 
             <button class="btn btn-lg btn-secondary btn-block mt-3" type="submit">Entrar</button>
         </div>
