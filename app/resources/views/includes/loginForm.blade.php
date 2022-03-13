603bdf7bc2c93d403c18b9562779023f469adab0
Stack-overflow-abuser <mmd.miguel@gmail.com>
2022-03-13 12:36:00 +0000

    melhoramento do form login

--- app/resources/views/includes/loginForm.blade.php
+++ app/resources/views/includes/loginForm.blade.php
@@ -5,11 +5,11 @@
 
             <h1 class="h3 mb-2 font-weight-normal">Autentique-se para usufruir  de todas as funcionalidades que temos para oferecer!</h1>
 
-            <label for="loginName" class="sr-only text-light">Nome de utilizador</label>
-            <input type="text" id="loginName" class="form-control form-control-sm mb-2" placeholder="Nome de utilizador" required="" autofocus="">
+            <label for="loginName" class="sr-only text-light"><h4>Nome de utilizador</h4></label>
+            <input type="text" id="loginName" class="form-control form-control-lg mb-2" placeholder="Nome de utilizador" required="" autofocus="">
 
-            <label for="password" class="sr-only text-light">Password</label>
-            <input type="password" id="password" class="form-control form-control-sm mb-2" placeholder="Password" required="" autofocus="">
+            <label for="password" class="sr-only text-light"><h4>Password</h4></label>
+            <input type="password" id="password" class="form-control form-control-lg mb-2" placeholder="Password" required="" autofocus="">
 
             <button class="btn btn-lg btn-secondary btn-block mt-3" type="submit">Entrar</button>
         </div>
