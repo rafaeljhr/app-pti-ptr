e5ff48aa43da052966e4fe4495c26ea42c437582
Stack-overflow-abuser <mmd.miguel@gmail.com>
2022-03-13 12:27:27 +0000

    Faltam retoques de design mas implementacao de formulario de login feita e mudanca e melhoramento de imports de css para serem mais especifico à pagina

---
+++ app/resources/views/includes/loginForm.blade.php
@@ -0,0 +1,17 @@
+<div class="container">
+    <form class="form-signin">
+        <div class="limit-width mx-auto">  
+            <img class="logo" src="images/logo.png" alt="EcoSmart Logo">
+
+            <h1 class="h3 mb-2 font-weight-normal">Autentique-se para usufruir  de todas as funcionalidades que temos para oferecer!</h1>
+
+            <label for="loginName" class="sr-only text-light">Nome de utilizador</label>
+            <input type="text" id="loginName" class="form-control form-control-sm mb-2" placeholder="Nome de utilizador" required="" autofocus="">
+
+            <label for="password" class="sr-only text-light">Password</label>
+            <input type="password" id="password" class="form-control form-control-sm mb-2" placeholder="Password" required="" autofocus="">
+
+            <button class="btn btn-lg btn-secondary btn-block mt-3" type="submit">Entrar</button>
+        </div>
+    </form>
+</div>
