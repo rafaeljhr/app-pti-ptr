9017f3054d989b7f24f1e33f4750f32bd64b4a62
Francisco Pimenta <xisco.pimenta@gmail.com>
2022-03-13 12:03:49 +0000

    Styling da página register

--- app/resources/views/includes/registerForm.blade.php
+++ app/resources/views/includes/registerForm.blade.php
@@ -1,23 +1,26 @@
 <div class="container">
     <form class="form-signin">
-        <img class="mb-4" src="images/logo.png" alt="EcoSmart Logo">
+        <div class="limit-width mx-auto">  
+            <img class="logo" src="images/logo.png" alt="EcoSmart Logo">
 
-        <h1 class="h3 mb-3 font-weight-normal">Junte-se ao grupo EcoSmart!</h1>
-        <label for="inputName" class="sr-only">Nome</label>
+            <h1 class="h3 mb-2 font-weight-normal">Junte-se ao grupo EcoSmart!</h1>
 
-        <input type="text" id="inputName" class="form-control" placeholder="João Carvalho" required="" autofocus="">
-        <label for="inputTel" class="sr-only">Telemóvel</label>
+            <label for="inputName" class="sr-only text-light">Nome</label>
+            <input type="text" id="inputName" class="form-control form-control-sm mb-2" placeholder="João Carvalho" required="" autofocus="">
+            
+            <label for="inputTel" class="sr-only text-light">Telemóvel</label>
+            <input type="text" id="inputTel" class="form-control form-control-sm mb-2" placeholder="987654321" size="9" required="" autofocus="">
 
-        <input type="number" id="inputTel" class="form-control" placeholder="987654321" size="9" required="" autofocus="">
-        <label for="inputNIF" class="sr-only">NIF</label>
+            <label for="inputNIF" class="sr-only text-light">NIF</label>
+            <input type="text" id="inputNIF" class="form-control form-control-sm mb-2" placeholder="123456789" size="9" required="" autofocus="">
 
-        <input type="number" id="inputNIF" class="form-control" placeholder="123456789" size="9" required="" autofocus="">
-        <label for="inputAdress" class="sr-only">Morada</label>
+            <label for="inputAdress" class="sr-only text-light">Morada</label>
+            <input type="text" id="inputAdress" class="form-control form-control-sm mb-2" placeholder="Rua Avenida Nº7 6ºE" maxlength="200" required="" autofocus="">
 
-        <input type="text" id="inputAdress" class="form-control" placeholder="Rua Avenida Nº7 6ºE" maxlength="200" required="" autofocus="">
-        <label for="password" class="sr-only">Password</label>
+            <label for="password" class="sr-only text-light">Password</label>
+            <input type="password" id="password" class="form-control form-control-sm mb-2" placeholder="Password" required="" autofocus="">
 
-        <input type="password" id="password" class="form-control" placeholder="Password" required="" autofocus="">
-        <button class="btn btn-lg btn-primary btn-block" type="submit">Registar</button>
+            <button class="btn btn-lg btn-secondary btn-block mt-3" type="submit">Registar</button>
+        </div>
     </form>
 </div>
