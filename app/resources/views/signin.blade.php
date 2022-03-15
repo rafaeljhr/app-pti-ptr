7afe7b308df1513a8cb96830e7295ec8d622238d
Stack-overflow-abuser <mmd.miguel@gmail.com>
2022-03-15 23:05:25 +0000

    implementacao do sistema de login, ficando a faltar a comunicação com DB

--- app/resources/views/signin.blade.php
+++ app/resources/views/signin.blade.php
@@ -4,6 +4,15 @@
 @section('background')
     
     @parent
+    @if ($errors->any())
+    <div class="alert alert-danger">
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+    @endif
     @include('includes.loginForm') 
     
 @endsection
