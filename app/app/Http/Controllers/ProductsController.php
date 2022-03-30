13d9ca3f5a205f760b4b7a6d28d62d0f8f3c6b1a
Rafael Ribeiro <rafajh.ribeiro@gmail.com>
2022-03-30 22:33:24 +0100

    Inicio do products controller

---
+++ app/app/Http/Controllers/ProductsController.php
@@ -0,0 +1,18 @@
+<?php
+
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Consumidor;
+use App\Models\Fornecedor;
+use App\Models\Transportadora;
+use Session;
+use Illuminate\Support\Facades\Hash;
+
+class ProductsController extends Controller
+{
+
+
+}
