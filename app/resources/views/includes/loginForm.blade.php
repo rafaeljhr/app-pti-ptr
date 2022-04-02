bb158ba5f87d9f53f25a975fd46b5313b4b500cc
Stack-overflow-abuser <mmd.miguel@gmail.com>
2022-04-02 18:24:59 +0100

    adicao de um dropdown para selecao de tipo de conta no sistema de login

--- app/resources/views/includes/loginForm.blade.php
+++ app/resources/views/includes/loginForm.blade.php
@@ -11,7 +11,15 @@
             <form class="p-4 p-md-5 border rounded-3 bg-light" method="post" action="{{ route('login-controller') }}">
                 @csrf
               <h1 class="h3 mb-2 font-weight-normal">Autentique-se para usufruir  de todas as funcionalidades da EcoSmart Store!</h1>
-              <div class="form-check form-check-inline">
+
+
+              <select class="form-select mb-3" name ="inlineRadioOptions" aria-label="Default select example">
+                <option selected>Selecione o tipo de conta</option>
+                <option value="consumidor">Consumidor</option>
+                <option value="transportadora">Transportadora</option>
+                <option value="fornecedor">Fornecedor</option>
+              </select>
+              {{-- <div class="form-check form-check-inline">
                 <input class="form-check-input" type="radio" name="inlineRadioOptions" id="consumidorSelect" value="consumidor">
                 <label class="form-check-label" for="inlineRadio1">Consumidor</label>
                 </div>
@@ -24,7 +32,7 @@
                 <div class="form-check form-check-inline">
                     <input class="form-check-input" type="radio" name="inlineRadioOptions" id="fornecedorSelect" value="fornecedor">
                     <label class="form-check-label" for="inlineRadio1">Fornecedora</label>
-                </div>
+                </div> --}}
 
               <div class="form-floating mb-3">
                 <input type="text" name="usernameLogin" id="loginName" class="form-control"  placeholder="Nome de utilizador">
