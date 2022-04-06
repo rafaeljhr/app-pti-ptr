56e74fa0269d71c44ca410773f45b0ed908ad2aa
Rafael Ribeiro <rafajh.ribeiro@gmail.com>
2022-04-06 02:46:37 +0100

    correcoes

--- api/app/Models/fornecedor.php
+++ api/app/Models/fornecedor.php
@@ -5,6 +5,8 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Laravel\Sanctum\HasApiTokens;
+use App\Models\Produto;
+use App\Models\Armazem;
 
 class fornecedor extends Model
 {
