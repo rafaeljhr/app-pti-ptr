3ff06ddee97cb4b248b096e11a56e9bf9b23feab
Stack-overflow-abuser <mmd.miguel@gmail.com>
2022-06-24 17:14:43 +0100

    detalhes de produtos melhorado mais uma vez

--- app/resources/views/produtos_detalhes.blade.php
+++ app/resources/views/produtos_detalhes.blade.php
@@ -11,7 +11,15 @@
 {{-- /////////////////////////////////////////////////////////////////// --}}
 <div id="apresentaçãoForm" class="mx-auto mt-4 mb-4">
 <h3 class="text-center">Detalhes do produto <?php echo session()->get('produto_detalhes')['produto_nome']?></h3>
-<p class="text-center"><img src="<?php echo session()->get('produto_detalhes')['produto_path_imagem']?>" ></p>
+<?php 
+    $image_path_filename = session()->get('produto_detalhes')['produto_path_imagem'];                
+    if (!file_exists(session()->get('produto_detalhes')['produto_path_imagem'])) {
+        $image_path_filename = "images/default_produto.jpg";
+    }
+                   
+?>
+
+<p class="text-center"><img src="{{$image_path_filename}}" ></p>
 <div class="row">
     <div class="col">
         <p>Preço: <?php echo session()->get('produto_detalhes')['produto_preco']?></p>
@@ -26,7 +34,47 @@
         <p>Informações adicionais: <?php echo session()->get('produto_detalhes')['produto_informacoes_adicionais']?></p>
     </div>
 </div>
+
+
+
+
+
+                    {{--   ////////////////////////////////////////////////////////// --}}
+                    {{-- ////////////////////////////////////////////////////////// --}}
+                    {{-- ////////////////////////////////////////////////////////// --}}
+                    {{-- ////////////////////////////////////////////////////////// --}}
+                    {{-- ////////////////////////////////////////////////////////// --}}
+                    {{-- ////////////////////////////////////////////////////////// --}}
+                    @if(Session::get('cadeias_produto_info') != [])
+                    
+
+                        <h3 class="mt-3 mb-5 text-center">Cadeias associadas ao produto</h3>
+
+                        <div class="row row-cols-1 row-cols-lg-4 row-cols-md-2 g-4">
+                          
+                            @for($i = 0; $i < sizeOf(session()->get('cadeias_produto_info')); $i++)
+                            <div class="col">
+                                <div class="evento-size card">
+                                  <h5 class="card-title"><?php echo session()->get('cadeias_produto_info')[$i]['evento_nome'] ?></h5>
+                                  <h4 class="card-text ">CO2 criados: <?php echo session()->get('cadeias_produto_info')[$i]['evento_co2'] ?></h4>             
+                                  <h4 class="card-text ">Kwh consumidos: <?php echo session()->get('cadeias_produto_info')[$i]['evento_kwh'] ?></h4>
+                                  <div class="card-body text-center">
+                                    <h5 class="card-title"><?php echo session()->get('cadeias_produto_info')[$i]['evento_desc'] ?></h5>
+                                  </div>
+                    
+                                </div>
+                              </div>
+                            @endfor
+                  
+                        
+                      </div>
+                      @else
+                      <h3 class="mt-3 mb-5 text-center">Este produto nao possui cadeias associadas</h3>
+                      @endif
 </div>
+
+
+
 </div>
 
 
