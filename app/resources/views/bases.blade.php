fad5748cfb77dbb7590d8962333672974958fca2
Rafael Ribeiro <rafajh.ribeiro@gmail.com>
2022-06-26 12:10:19 +0100

    Correcao de bases e veiculos. Comprimidas as imagens principais do site, por forma a reduzir os tempos de loading do site drasticamente.

--- app/resources/views/bases.blade.php
+++ app/resources/views/bases.blade.php
@@ -19,88 +19,91 @@
   <div id="fundoDivOpac"  class="backgroundSee"></div>
 
  
-  <div id="apresentação" class="mx-auto mt-4 mb-4">
   
-    @if(session()->get('bases') != null)
-    <div class="container p-0 mt-5 mb-5">
+  @if(session()->get('bases') != null)
+  <div class="container p-0 mt-5 mb-5">
 
-      <div class="row w-100 mt-4 mb-4">
+    <div class="row w-100 mt-4 mb-4">
 
-        <div class="col">
-          <h1>As suas bases</h1>
-        </div>
-        <div class="col-auto">
-            <button class="btn btn-success" @click="criar()" id="botao_criar">Criar base</button>
-        </div>
-        
+      <div class="col">
+        <h1>As suas bases</h1>
       </div>
-
-      <div id='todasBases'>
-        <div class="row row-cols-1 row-cols-lg-4 row-cols-md-2 g-4">
-          
-            @for($i = 0; $i < sizeOf(session()->get('bases')); $i++)
-              <div class="col">
-                <div class="card">
-                  <img class="card-img-top imagem_da_card" src='<?php echo session()->get('bases')[$i]['base_path_imagem'] ?>'>
-
-                  <h4 class="card-title mt-3 text-center"><?php echo session()->get('bases')[$i]['base_nome'] ?></h4>
-
-                  <div class="card-body text-center">
-                    <div class="row">
-                      <div class="col">
-                        <p>
-                          <?php
-  
-                            $num_veiculos = 0;
-                            foreach (session()->get('veiculos') as $veiculo) {
-  
-                              if ($veiculo['veiculo_id_base'] == session()->get('bases')[$i]['base_id']) {
-                                $num_veiculos += $veiculo['veiculo_quantidade'];
-                              }
-  
-                            }
-  
-                            echo 'Nº de veículos na base: ' . $num_veiculos;
-  
-                          ?>
-                        </p>
-                      </div>
-                    </div>
-                  
-                    <a href="{{ URL::to('base/'.session()->get('bases')[$i]['base_id']) }}">
-                      <button type="button" class="btn btn-info botoes_veiculos">Detalhes da base</button>
-                    </a>
-                  </div>
-    
-                </div>
-              </div>
-            @endfor
-  
-        </div>
-      </div>
-    </div>
-
-    @else
-
-    <div id="noBases">
-
-      <div align="center" class="mx-auto">
-        <img src="images/armazens.png" class="sem_bases_img" alt="">
-        <br>
-        <br>
-        <h2>Parece que não possui bases.</h2>
-        <p>As bases são necessárias para possuir veículos, então crie uma primeiramente!</p>
-        <br>
-        <button class="btn btn-success" @click="criar()" id="botao_criar">Criar base</button>
+      <div class="col-auto">
+          <button class="btn btn-success" @click="criar()" id="botao_criar">Criar base</button>
       </div>
       
-
     </div>
 
+    <div id='todasBases'>
+      <div class="row row-cols-1 row-cols-lg-4 row-cols-md-2 g-4">
+        
+          @for($i = 0; $i < sizeOf(session()->get('bases')); $i++)
+            <div class="col">
+              <div class="card">
 
-    @endif
+
+                @if(!file_exists(session()->get('bases')[$i]['base_path_imagem']))
+                  <img class="card-img-top imagem_da_card" src='images/default_base.jpg'>
+                @else
+                  <img class="card-img-top imagem_da_card" src='<?php echo session()->get('bases')[$i]['base_path_imagem'] ?>'>
+                @endif
+
+                <h4 class="card-title mt-3 text-center"><?php echo session()->get('bases')[$i]['base_nome'] ?></h4>
+
+                <div class="card-body text-center">
+                  <div class="row">
+                    <div class="col">
+                      <p>
+                        <?php
+
+                          $num_veiculos = 0;
+                          foreach (session()->get('veiculos') as $veiculo) {
+
+                            if ($veiculo['veiculo_id_base'] == session()->get('bases')[$i]['base_id']) {
+                              $num_veiculos += $veiculo['veiculo_quantidade'];
+                            }
+
+                          }
+
+                          echo 'Nº de veículos na base: ' . $num_veiculos;
+
+                        ?>
+                      </p>
+                    </div>
+                  </div>
+                
+                  <a href="{{ URL::to('base/'.session()->get('bases')[$i]['base_id']) }}">
+                    <button type="button" class="btn btn-info botoes_veiculos">Detalhes da base</button>
+                  </a>
+                </div>
+  
+              </div>
+            </div>
+          @endfor
+
+      </div>
+    </div>
+  </div>
+
+  @else
+
+  <div id="noBases">
+
+    <div align="center" class="mx-auto">
+      <img src="images/armazens.png" class="sem_bases_img" alt="">
+      <br>
+      <br>
+      <h2>Parece que não possui bases.</h2>
+      <p>As bases são necessárias para possuir veículos, então crie uma primeiramente!</p>
+      <br>
+      <button class="btn btn-success" @click="criar()" id="botao_criar">Criar base</button>
+    </div>
+    
 
   </div>
+
+
+  @endif
   
   <div id="criar" class="base">
     <button type="button" onclick="criar()" class="btn-close" id="button-close-div"  aria-label="Close"></button>
